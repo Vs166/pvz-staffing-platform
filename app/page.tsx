0f1b7a759289ab6fa17ccf1bf8bfb9f0fec657d8
Вадим Шакин <vs@MacBook-Pro-Vadim.local>
2025-04-11 17:07:20 +0300

    Создан файл page.tsx в app/

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+ 2 files changed, 16 insertions(+), 110 deletions(-)
+ delete mode 100644 app/page.tsx
+ create mode 100644 app/page.tsx.rtf
+Enumerating objects: 6, done.
+Counting objects: 100% (6/6), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (4/4), done.
+Writing objects: 100% (4/4), 795 bytes | 795.00 KiB/s, done.
+Total 4 (delta 2), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (2/2), completed with 2 local objects.
+To https://github.com/Vs166/pvz-staffing-platform.git
+   fac1690..7ac919e  main -> main
+vs@MacBook-Pro-Vadim pvz-staffing-platform % 
